b9bd0e779ef9ef24be7a81d3cd8de45e168401f0
Debraj <debrajd1983@gmail.com>
2024-06-14 15:01:59 +0530

    Main class modified

--- src/main/java/com/example/DepartmentServiceApplication.java
+++ src/main/java/com/example/DepartmentServiceApplication.java
@@ -9,6 +9,7 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 public class DepartmentServiceApplication {
 
 	public static void main(String[] args) {
+		System.out.println("Spring boot main file");
 		SpringApplication.run(DepartmentServiceApplication.class, args);
 	}
 
