ca1a9b7d7f6cd2032a09aaec2a9bc2003f364f11
Debraj <debrajd1983@gmail.com>
2024-06-17 18:16:59 +0530

    301

--- src/main/java/com/example/DepartmentServiceApplication.java
+++ src/main/java/com/example/DepartmentServiceApplication.java
@@ -7,11 +7,13 @@ import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 @SpringBootApplication
 @EnableDiscoveryClient
 public class DepartmentServiceApplication {
+	String s="";
 
 	public static void main(String[] args) {
 		System.out.println("Spring boot main file");
 		//changes for HLS-132
 		SpringApplication.run(DepartmentServiceApplication.class, args);
+		//changes done here
 	}
 
 }
